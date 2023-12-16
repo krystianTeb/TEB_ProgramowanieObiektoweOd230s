95ff03e2a94c28795be8d51773c3d5038b59f150
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-16 09:35:09 +0100

    3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,19 @@ package org.example;
 
 import java.util.Scanner;
 
-class Punkt{
-    int x; //To jest właściwość (polami) klasy
-    int y; //To jest właściwość (polami) klasy
-}
-
 public class Main {
     public static void main(String[] args) {
 
+        int liczba = 5;
+
         Punkt punkt1 = new Punkt();//Utworzenie obiektu (instancji) klasy Punkt
         punkt1.x = 5;//Przypisujemy 5 do właściwości x w obiekcie punkt1
-        System.out.println("Właściwość x obiektu punkt1: " + punkt1.x +
-                            "\nWłaściwość y obiektu punkt1: " + punkt1.y);
+        punkt1.wypiszPunkty();
+
         Punkt punkt2 = new Punkt();//Utworzenie obiektu (instancji) klasy Punkt
-        System.out.println("Właściwość x obiektu punkt2: " + punkt2.x +
-                            "\nWłaściwość y obiektu punkt2: " + punkt2.y);
+        punkt2.wypiszPunkty();
+
         Punkt punkt3 = new Punkt();//Utworzenie obiektu (instancji) klasy Punkt
-        System.out.println("Właściwość x obiektu punkt3: " + punkt3.x +
-                "\nWłaściwość y obiektu punkt3: " + punkt3.y);
+        punkt3.wypiszPunkty();
     }
 }
---
+++ src/main/java/org/example/Punkt.java
@@ -0,0 +1,10 @@
+package org.example;
+
+class Punkt{
+    int x; //To jest właściwość (polami) klasy
+    int y; //To jest właściwość (polami) klasy
+    void wypiszPunkty(){ //To jest metoda, która wypisuje właściwości obiektu
+        System.out.println("Właściwość x: " + this.x +
+                "\nWłaściwość y: " + this.y);
+    }
+}
