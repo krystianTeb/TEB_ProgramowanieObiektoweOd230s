b49786039828a99efab8816c33ba2e20ae7d807e
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-16 11:49:20 +0100

    8+cw1_p1

---
+++ src/main/java/org/example/LiczbaCalkowita.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class LiczbaCalkowita {
+    public int liczba;
+    public String wyswietlLiczbe(){
+        return "Liczba zapisana: " + liczba;
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        LiczbaCalkowita liczba = new LiczbaCalkowita();
+        liczba.liczba = 5;
+        System.out.println( liczba.wyswietlLiczbe() );
 
-        int liczba = 5;
 
-        Punkt punkt1 = new Punkt(5, 2);//Utworzenie obiektu (instancji) klasy Punkt
-        System.out.println( punkt1 );
 
-        Punkt punkt2 = new Punkt(8,8);//Utworzenie obiektu (instancji) klasy Punkt
-        System.out.println( punkt2 );
-        System.out.println( punkt2.getX() );
 
     }
 }
