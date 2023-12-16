1e3a5261794b54b8e7ac2be7451ff4116451b8ff
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-16 12:07:25 +0100

    10_cw2_2czesc_final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Prostokat prostokat1 = new Prostokat();
-        prostokat1.punkt1 = new Punkt();
-        prostokat1.punkt1.x = 1;
-        prostokat1.punkt1.y = 1;
-
-
+        prostokat1.punkt1 = new Punkt(1, 1);
+        prostokat1.punkt2 = new Punkt(2, 2);
+        prostokat1.punkt3 = new Punkt(3, 3);
+        prostokat1.punkt4 = new Punkt(4, 4);
+        prostokat1.punkt1.wyswietlPunkt();
+        prostokat1.punkt2.wyswietlPunkt();
+        prostokat1.punkt3.wyswietlPunkt();
+        prostokat1.punkt4.wyswietlPunkt();
 
 
     }
--- src/main/java/org/example/Punkt.java
+++ src/main/java/org/example/Punkt.java
@@ -2,4 +2,12 @@ package org.example;
 class Punkt{
     public int x; //To jest właściwość (polami) klasy
     public int y; //To jest właściwość (polami) klasy
+
+    public Punkt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void wyswietlPunkt(){
+        System.out.println("(" + this.x + ", " + this.y + ")");
+    }
 }
