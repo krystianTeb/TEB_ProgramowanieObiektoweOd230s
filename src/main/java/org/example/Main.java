0f2b48e0b769060403ad0e8cf8c2f45353d3b37a
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-16 09:15:29 +0100

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
+class Punkt{
+    int x; //To jest właściwość (polami) klasy
+    int y; //To jest właściwość (polami) klasy
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in); //Tworzymy nowy obiekt klasy Scaner
+        int liczba = 5; //Utworzenie zmiennej
+
+
+        System.out.println("Potęgowanie: " + Math.pow(2, 2));//Metoda matematyczna pow()
+        System.out.println("Wartość PI: " + Math.PI);//Właściwość klasy matematycznej
     }
 }
